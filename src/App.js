fbfba3b7cc3a41455b53bd573160f9f174753143
Shakhzod Kholmonov <shakhzodkholmonov@gmail.com>
2024-10-20 17:47:17 +0500

    ff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const botToken = "Bot tokenini qoying";
+    const chatId = "chat idsini qo'ying";
+    const message = `Sizda yangi xabar bor:\nIsm: ${data.name}\nNumber ${data.number}\nEmail: ${data.email}\nQo'shimcha Ma'lumot: ${data.description}`;
+
+    try {
+      const response = await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+        chat_id: chatId,
+        text: message,
+      });
+      console.log("Xabar yuborildi:", response.data);
+      reset();
+    } catch (error) {
+      console.error("Xabar yuborishda xatolik bor!:", error.response ? error.response.data : error.message);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label>Ism</label>
+        <input type="text" {...register("name", { required: "Ism kiritish majburiy" })} />
+        {errors.name && <p>{errors.name.message}</p>}
+      </div>
+      <div>
+        <label>Raqam</label>
+        <input typr="text" {...register("number", { required: "Raqam kiritilishi majburiy" })} />
+        {errors.name && <p>{errors.number.message}</p>}
+      </div>
+
+      <div>
+        <label>Email</label>
+        <input type="email" {...register("email", { required: "Email kiritish majburiy" })} />
+        {errors.email && <p>{errors.email.message}</p>}
+      </div>
+
+      <div>
+        <label>Qo'shimcha Ma'lumot</label>
+        <textarea {...register("description")} placeholder="Qo'shimcha ma'lumot kiriting"></textarea>
+      </div>
+
+      <button type="submit">Yuborish</button>
+    </form>
   );
-}
+};
 
 export default App;
